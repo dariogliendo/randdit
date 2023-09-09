0a283fc4599fa64f847f90dfb42ef175528a84f7
dliendo <dario.liendo@gmail.com>
2023-09-09 20:56:13 -0300

    server modularization and other features

---
+++ api/posts.ts
@@ -0,0 +1,20 @@
+import axios from 'axios'
+import { Router } from 'express'
+import { RequestHandler, NextFunction } from 'express'
+
+const Posts = Router()
+
+const posts: RequestHandler = async (req, res, next: NextFunction): Promise<void> => {
+  try {
+    const { data } = await axios.get(`https://oauth.reddit.com/api/v1/r/${req.query.subreddit}/new`, {
+
+    })
+    res.send(data)
+  } catch (error) {
+    next(error)
+  }
+}
+
+Posts.get('/api/posts', posts)
+
+export default Posts
